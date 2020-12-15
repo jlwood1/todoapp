d30cd939532c5dd23437ac4c65db49dfc2ccafe8
jlwood1 <woodringj99@gmail.com>
2020-12-14 22:56:57 -0500

    working todoapp

---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../models/Todo'
+import { TodoService } from '../../services/todo.service'
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.scss']
+})
+export class TodosComponent implements OnInit {
+  todos:Todo[];
+
+  constructor(private todoService:TodoService) { }
+
+  ngOnInit(): void {
+    this.todoService.getTodos().subscribe(todos => {
+      this.todos = todos; 
+    });
+  }
+
+  deleteTodo(todo:Todo) {
+    //Remove from ui 
+    this.todos = this.todos.filter(t => t.id !== todo.id);
+    //Remove from server
+    this.todoService.deleteTodo(todo).subscribe();
+  }
+  
+  addTodo(todo:Todo) {
+    let newId = todo.id ? todo.id++ : undefined
+    
+    this.todoService.addTodo(todo).subscribe(todo => {
+      todo.id = newId
+      this.todos.push(todo);
+    })
+    console.log(this.todos)
+  }
+}
